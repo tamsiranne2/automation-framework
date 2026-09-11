c7f83e83d2d894de56322a411ca140ce67a45d4e
tamsir <tamsir.anne@hotmail.de>
2026-09-11 18:02:47 +0000

    initial commit

---
+++ automation-framework/src/test/java/com/mycompany/test/AppTest.java
@@ -0,0 +1,13 @@
+package com.mycompany.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void sampleTest() {
+        System.out.println("Running automated test inside Maven framework!");
+        assertTrue(true);
+    }
+}
